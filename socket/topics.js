b46e09e816ee205440f3040df6f260e89b1f76f7
Nicolas <nicolas.droesch@gmail.com>
2024-06-07 17:31:36 +0200

    Remise au propre du code Fonction atomique Fonction explicite Suppression des commentaires Ajout d'un controller xbee Déplacement des fonctions de gestion des xbee Mise a jour du READ.ME

--- socket/topics.js
+++ socket/topics.js
@@ -3,8 +3,9 @@ const Topics = Object.freeze({
     PHOTO_RES0: 'topic/esiee/escape/photo_res0',
     PHOTO_RES1: 'topic/esiee/escape/photo_res1',
     PHOTO_RES2: 'topic/esiee/escape/photo_res2',
-    LED: 'topic/esiee/escape/led',
-    PRESSURE: 'topic/esiee/escape/pressure'
-});
-
-module.exports = Topics;
+    LED:        'topic/esiee/escape/led',
+    PRESSURE:   'topic/esiee/escape/pressure'
+  });
+  
+  module.exports = Topics;
+  
